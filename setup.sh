977bcd341deb889e833502ed0fb4e49d3d72b9fd
Rajesh Roy <rajeshroy402@gmail.com>
2022-07-21 17:59:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/marcoslucianops/DeepStream-Yolo.git
 cp ip_rajesh/* DeepStream-Yolo/
-cd yolov3 && wget /https://pjreddie.com/media/files/yolov3.weights
+cd yolov3 && wget https://pjreddie.com/media/files/yolov3.weights
 cd ../yolov4 && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
 echo "done! thank you!"
